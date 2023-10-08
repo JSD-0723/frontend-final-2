a19f0afb15b4a09d5e59d2c9d5493f9fda6c0e77
Rayah Dwikat <rayah.hamdan95@gmail.com>
2023-10-09 01:03:55 +0300

    new changes

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,23 +1,36 @@
 import { AppBar, Box, Toolbar, Typography } from '@mui/material';
 import * as React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
 
 
- 
 
 export default function Header() {
-  
+
   return (
-  <Box>
-  <AppBar position="sticky">
-  <Toolbar>
-    <Typography variant='h4'>CORAL'S</Typography> 
-    <Typography sx={{marginRight : 2}}>HandBags</Typography>
-    <Typography sx={{marginRight : 2}}>Watches</Typography>
-    <Typography sx={{marginRight : 2}}>SkinCare</Typography>
-    <Typography sx={{marginRight : 2}}>Jewellary</Typography>
-    <Typography sx={{marginRight : 2}}>Apperels</Typography>
-  </Toolbar>
-  </AppBar>
-  </Box> 
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="sticky" sx={{backgroundColor:'white'}}>
+        <Toolbar >
+          <Box sx={{ width: '108 px', height: '22 px'}}>
+            <Typography variant='h4' sx={{ marginRight: 4 , color:'#17494D',  fontFamily: 'Jost, sans-serif', fontWeight: 'bold'}}>
+              CORA'L</Typography>
+          </Box>
+          <Box sx={{ display: 'flex' , color:'#171520', fontFamily: 'Inter, sans-serif'}}>
+            <Typography sx={{ marginRight: 2, fontFamily: 'Inter, sans-serif' }}>HandBags</Typography>
+            <Typography sx={{ marginRight: 2, fontFamily: 'Inter, sans-serif' }}>Watches</Typography>
+            <Typography sx={{ marginRight: 2, fontFamily: 'Inter, sans-serif' }}>SkinCare</Typography>
+            <Typography sx={{ marginRight: 2, fontFamily: 'Inter, sans-serif'}}>Jewellary</Typography>
+            <Typography sx={{ marginRight: 2, fontFamily: 'Inter, sans-serif'}}>Apperels</Typography>
+          </Box>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon/>
+            </SearchIconWrapper>
+            <StyledInputBase
+               placeholder="search for products or brands....." 
+               inputProps={{'area-label':'search'}}/>
+          </Search>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
