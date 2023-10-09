b59bff0a49deae6b285964e45dd32c9636d1ddcc
Rayah Dwikat <rayah.hamdan95@gmail.com>
2023-10-10 02:01:54 +0300

    new changes0

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,31 +5,39 @@ import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 
 export default function SearchBar() {
-  return (
-    <Paper
-      component="form"
-      sx={{ p: '2px 4px',
-       display: 'flex',
-        alignItems: 'center',
-          width: '400px',
-          height: '44px',
-          borderColor:'black',
-          borderRadius: '4px',
-          marginLeft: '170px',
-          color:'#626262',
-          backgroundColor:'#F1F1F1',
-          fontFamily: 'Inter, sans-serif',
-          boxShadow: 'none'
-        }}
-    >
-   <IconButton type="button" aria-label="search">
-        <SearchIcon />
-      </IconButton> 
-      <InputBase
-        placeholder="Search for products or brands...."
-        inputProps={{ 'aria-label': 'Search for products or brands....' }}
-      />
-         
-    </Paper>
-  );
+    return (
+        <Paper
+            component="form"
+            sx={{
+                p: '2px 4px',
+                display: 'flex',
+                alignItems: 'center',
+                width: '400px',
+                height: '44px',
+                borderColor: 'black',
+                borderRadius: '4px',
+                marginLeft: '170px',
+                color: '#626262',
+                backgroundColor: '#F1F1F1',
+                boxShadow: 'none'
+
+            }}
+        >
+            <IconButton type="button" aria-label="search">
+                <SearchIcon />
+            </IconButton>
+            <InputBase sx={{
+                fontFamily: 'Inter, sans-serif',
+                fontSize: '14px',
+                fontWeight: '500',
+                lineHeight: '18px',
+                width: '221px',
+                height: '18px'
+            }}
+                placeholder="Search for products or brands...."
+                inputProps={{ 'aria-label': 'Search for products or brands....' }}
+            />
+
+        </Paper>
+    );
 }
