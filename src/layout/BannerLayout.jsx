1c2a6bcab4764b5420b5f1ae735f19d26d8664bb
Rayah Dwikat <rayah.hamdan95@gmail.com>
2023-10-10 21:47:55 +0300

    new changes

--- src/layout/BannerLayout.jsx
+++ src/layout/BannerLayout.jsx
@@ -1,42 +1,47 @@
-import { Typography } from "@mui/material";
-import { Box } from "@mui/material";
+
+import { Typography, Box, Grid, Hidden } from "@mui/material";
 
 export function BannerLayout() {
     return (
-        <Box sx={{
-            height: '50px',
-            backgroundColor: '#F0F0F0'
-        }}>
-            <Typography variant="h4" sx={{
-                fontFamily: 'Inter, sans-serif',
-                fontWeight: '400',
-                fontSize: '14px',
-                lineHeight: '18px',
-                alignItems: 'center',
-                color: '#171520',
-                justifyContent: 'center',
-                marginLeft:'250px'
-            }} >
-                We are currently experiencing local customs clearance delays. For the latest updates, please check your order status 
-
-                <Typography variant="h4"
+        <Hidden mdDown>
+            <Grid
+                container
+                sx={{
+                    height: '50px',
+                    backgroundColor: '#F0F0F0',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    textAlign: 'center',
+                }}
+            >
+                <Typography
+                    variant="h4"
+                    sx={{
+                        fontFamily: 'Inter, sans-serif',
+                        fontWeight: '400',
+                        fontSize: '14px',
+                        lineHeight: '18px',
+                        color: '#171520',
+                        marginRight: '4px',
+                    }}
+                >
+                    We are currently experiencing local customs clearance delays. For the latest updates, please check your order status
+                </Typography>
+                <Typography
+                    variant="h4"
                     sx={{
                         fontFamily: 'Inter, sans-serif',
                         fontWeight: '500',
                         fontSize: '14px',
                         lineHeight: '18px',
-                        alignItems: 'center',
                         color: '#1B4B66',
-                        justifyContent: 'center'
+                        textDecoration: 'underline',
+                        textDecorationColor: '#1B4B66'
                     }}
-                    >
-                        here
-                    </Typography>
-            </Typography>
-        </Box>
-
-    )
-
-
-
+                >
+                    here
+                </Typography>
+            </Grid>
+        </Hidden>
+    );
 }
