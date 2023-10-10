12d889650c6f7d7fcd1dcd7e729fe17ea10c77af
Elyas <Elyas575@hotmail.com>
2023-10-10 10:41:36 +0300

    refactored code

---
+++ src/layout/footer/FooterStyles.jsx
@@ -0,0 +1,41 @@
+
+export const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+    gap: 5,
+    backgroundColor: "#1B4B66",
+    fontSize: 14,
+    p: 3,
+    fontFamily: "Inter",
+  },
+  gridItem: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 1,
+  },
+  link: {
+    color: "#B6B6B6",
+    textDecoration: "none",
+  },
+  heading: {
+    color: "#FFFFFF",
+    textDecoration: "none",
+    fontSize: 16,
+  },
+  divider: {
+    backgroundColor: "#FFFFFF",
+    marginBottom: 2,
+    marginTop: 2,
+  },
+  location: {
+    fontSize: 14,
+    color: "#FFFFFF",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    lineHeight: 2,
+    gap: 5,
+  },
+};
