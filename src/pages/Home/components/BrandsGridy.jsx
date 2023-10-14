549cff8274633355d3e18308f3f0c416c57a6757
Elyas <Elyas575@hotmail.com>
2023-10-14 18:31:19 +0300

    Shop By brand complete

--- src/pages/Home/components/BrandsGridy.jsx
+++ src/pages/Home/components/BrandsGridy.jsx
@@ -1,32 +1,27 @@
 import { Box, Grid } from "@mui/material";
+import loo1 from "../../../helper/Assets/logo1.png";
+import loo2 from "../../../helper/Assets/logo2.png";
+import loo3 from "../../../helper/Assets/logo3.png";
+import loo4 from "../../../helper/Assets/logo4.png";
+import loo5 from "../../../helper/Assets/logo5.png";
+import "./styles.css";
+import { useMediaQuery } from "@mui/material";
 
 const BrandsGrid = () => {
+  const isMobile = useMediaQuery("(max-width:600px)");
+  const logos = [loo1, loo1, loo2, loo3, loo4, loo5];
   return (
-    <Box sx={{ width: "92%", marginLeft: 2 }}>
-      <Grid container spacing={6}>
-        <Grid item sm={4} md={2}>
-          LOGO
+    <Grid container>
+      {logos.map((logo) => (
+        <Grid item xs={4} md={2}>
+          <img
+            className={`image ${isMobile ? "mobile" : "desktop"}`}
+            src={logo}
+            alt=""
+          />
         </Grid>
-
-        <Grid item sm={4} md={2}>
-          LOGO
-        </Grid>
-
-        <Grid item sm={4} md={2}>
-          LOGO
-        </Grid>
-
-        <Grid item sm={4} md={2}>
-          LOGO
-        </Grid>
-        <Grid item sm={4} md={2}>
-          LOGO
-        </Grid>
-        <Grid item sm={4} md={2}>
-          LOGO
-        </Grid>
-      </Grid>
-    </Box>
+      ))}
+    </Grid>
   );
 };
 
