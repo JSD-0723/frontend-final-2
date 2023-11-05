4c80091b205ffc458c71bbe936a5c4f41b6e7669
Elyas <Elyas575@hotmail.com>
2023-11-05 13:33:13 +0200

    arrows for images in category page fixed

--- src/pages/Product/uppersection/ImagesSlider.jsx
+++ src/pages/Product/uppersection/ImagesSlider.jsx
@@ -1,9 +1,8 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import "./styles.css";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-
 const ImagesSlider = ({ images, next, prev }) => {
   return (
     <Box
@@ -11,17 +10,24 @@ const ImagesSlider = ({ images, next, prev }) => {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        marginTop: 3,
+        marginTop: 2,
       }}
     >
-      <button onClick={next}>
-        <KeyboardArrowLeftIcon />{" "}
-      </button>
+      <Button onClick={next} variant="text">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 0 24 24"
+          width="24"
+        >
+          <path d="M0 0h24v24H0V0z" fill="none" />
+          <path d="M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z" />
+        </svg>
+      </Button>
 
       <Box
         sx={{
           display: "flex",
-          justifyContent: "space-between",
           alignItems: "center",
         }}
       >
@@ -29,9 +35,18 @@ const ImagesSlider = ({ images, next, prev }) => {
           return <img className="small-image" src={image} alt="" />;
         })}
       </Box>
-      <button onClick={prev}>
-        <KeyboardArrowRightIcon />
-      </button>
+
+      <Button onClick={prev} variant="text">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24"
+          viewBox="0 0 24 24"
+          width="24"
+        >
+          <path d="M0 0h24v24H0V0z" fill="none" />
+          <path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z" />
+        </svg>
+      </Button>
     </Box>
   );
 };
